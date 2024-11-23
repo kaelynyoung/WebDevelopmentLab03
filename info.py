4795556cdbf2589c625a097fd63a9d55fc0ef440
kaelynyoung <kaelynamaya05@gmail.com>
2024-11-23 16:58:02 -0500

    Add files via upload

---
+++ info.py
@@ -0,0 +1,77 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "Images/kaelynProfilePic.jpeg"
+about_me = "I'm Kaelyn Young. My major is Atmospheric and Oceanic Sciences. I work as an UGTA for EAS 2600. "
+home_pro_pic = "blueclouds.jpg"
+homepage_words = "Hello! This is Team 54 Section D's Homepage! Here you can find links to our profiles, and a link about movies! Enjoy!"
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/kaelyn-young/"
+my_github_url = "https://github.com/kaelynyoung/WebDevelopmentLab03.git"
+my_email_address = "kyoung308@gatech.edu"
+
+
+education_data ={ 
+    'Degree': 'Bachelor of Science in Atmospheric and Oceanic Science',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': 'May 2027',
+    'GPA': '3.5'
+}
+course_data = {
+    "code":["CS 1301", "KOR 2001", "MATH 1553", "EAS 4350"], 
+    "names":["Intro to CS", "Intermediate Korean I", "Linear Algebra", "Paleoclimatology and Paleoceanography"], 
+    "semester_taken":["3rd", "3rd", "3rd", "3rd"],
+    "skills":["Python is not my favorite snake anymore", "I <3 Korean", "Basically row reduction", "My favorite class ever!"],
+    }
+experience_data = {
+    "Cook at Krusty Krab": (["- Worked on designing the Krabby Patty Recipe",
+                                                                          "- Increased Company Revenue by 200 dollars", "- Made Mr. Krabs Proud"],"Images/cook.jpg"),
+    "Cleaner at Krusty Krab":(["- Made the floor almost reflective",
+                                                           "- Increased customer visits by three folds by making restaurant clean"],"Images/cleaner.jpg"),
+    "Jelly Fish Catcher":(["- Caught 100+ Jellyfish with the help of my colleague Patrick"],"Images/jelly.jpg")
+
+}
+
+projects_data = {
+    "Krabby Patty Recipe": "Created the famous Recipe and tailored it from scratch",
+}
+
+programming_data = {
+    "Python": 90,
+    "Java": 70,
+    "C": 40,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+    "Java": "☕",
+    "C": "🔍",
+}
+spoken_icons = {"French": "🇫🇷",
+    "English": "🇬🇧",
+    "Spanish":"🇪🇸"
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "Korean": "Intermediate",
+    "Spanish": "Basics",
+}
+leadership_data = {
+    "Driver at Mrs Puff's School": (["- Elected Worst Student of the Century"],"Images/puff.jpg"),
+
+}
+activity_data={
+    "SpongeBob and Patrick Friendship Club": ["- Conviced Squidwards to have fun", 
+            "- Sat under Patrick's rock"]
+}
